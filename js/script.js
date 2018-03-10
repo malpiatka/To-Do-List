31e50ae0ce7e96730789e58dde3d4fa966548c42
Magdalena Mielke <magdalena.mielke90@gmail.com>
2018-03-10 11:50:45 +0100

    upgrade

--- js/script.js
+++ js/script.js
@@ -1,17 +1,63 @@
 //changing color and text decoration -long version
-
-$("li").click(function(){
-	if ($(this).css("color") === "rgb(128, 128, 128)"){
+/*
+$("ul").on("click", "li", function () {
+	if ($(this).css("color") === "rgb(128, 128, 128)") {
 		$(this).css({
-		color: "black",
-		textDecoration: "none"
+			color: "black",
+			textDecoration: "none"
 		});
-	}
-	else
-	$(this).css({
-		color: "gray",
-		textDecoration: "line-through"
+	} else
+		$(this).css({
+			color: "gray",
+			textDecoration: "line-through"
+		});
+});
+*/
+
+//changing color and text decoration -short version
+	$("ul").on("click", "li",function(){
+		$(this).toggleClass("done")
+		});
+
+
+//click an x to delete todo element with fading out
+$("ul").on("click", ".delete", function(event){
+	$(this).parent().fadeOut(500, function(){
+	$(this).remove();
 	});
+	event.stopPropagation();
 });
 
 
+//adding todo elements to list
+$("input[type='text']").keypress(function (event) {
+	if (event.which === 13) {
+		let todoAdd = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span class='check'><i class='fas fa-check'></i></span>" + todoAdd + "<span class='delete'><i class='fas fa-times'></i></span></li>");
+	}
+});
+/* 
+//not working ??
+$("ul").on("click", ".add", function(){
+	let todoAdd = $(this).val();
+	$("ul").append("<li><span class='check'><i class='fas fa-check'></i></span>"+todoAdd+"<span class='delete'><i class='fas fa-times'></i></span></li>");
+});
+*/
+//adding todo elements to list by click and enter
+/*
+//click not working
+$("input[type='text']").ready(function() {
+	$(".add").click(function(){
+		let todoAdd = $(this).val();
+		$("ul").append("<li><span class='check'><i class='fas fa-check'></i></span>"+todoAdd+"<span class='delete'><i class='fas fa-times'></i></span></li>");
+		});
+		$("input[type='text']").keypress(function(event){
+			if(event.which === 13){
+				let todoAdd = $(this).val();
+				$(this).val("");
+				$("ul").append("<li><span class='check'><i class='fas fa-check'></i></span>"+todoAdd+"<span class='delete'><i class='fas fa-times'></i></span></li>");
+			}
+	});
+});
+*/
