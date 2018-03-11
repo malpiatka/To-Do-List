bca99eb9d7c6e8c93f1758c46878a8d1ec83816d
Magdalena Mielke <magdalena.mielke90@gmail.com>
2018-03-11 23:10:46 +0100

    js and css changes

--- js/script.js
+++ js/script.js
@@ -1,23 +1,8 @@
-//changing color and text decoration -long version
-/*
-$("ul").on("click", "li", function () {
-	if ($(this).css("color") === "rgb(128, 128, 128)") {
-		$(this).css({
-			color: "black",
-			textDecoration: "none"
-		});
-	} else
-		$(this).css({
-			color: "gray",
-			textDecoration: "line-through"
-		});
+//changing color and text decoration
+$("ul").on("click", ".check", function(event){
+	$(this).parent().toggleClass("done")
+	event.stopPropagation();
 });
-*/
-
-//changing color and text decoration -short version
-	$("ul").on("click", "li",function(){
-		$(this).toggleClass("done")
-		});
 
 
 //click an x to delete todo element with fading out
@@ -28,28 +13,11 @@ $("ul").on("click", ".delete", function(event){
 	event.stopPropagation();
 });
 
-
-//adding todo elements to list
-$("input[type='text']").keypress(function (event) {
-	if (event.which === 13) {
-		let todoAdd = $(this).val();
-		$(this).val("");
-		$("ul").append("<li><span class='check'><i class='fas fa-check'></i></span>" + todoAdd + "<span class='delete'><i class='fas fa-times'></i></span></li>");
-	}
-});
-/* 
-//not working ??
-$("ul").on("click", ".add", function(){
-	let todoAdd = $(this).val();
-	$("ul").append("<li><span class='check'><i class='fas fa-check'></i></span>"+todoAdd+"<span class='delete'><i class='fas fa-times'></i></span></li>");
-});
-*/
 //adding todo elements to list by click and enter
-/*
-//click not working
 $("input[type='text']").ready(function() {
 	$(".add").click(function(){
-		let todoAdd = $(this).val();
+		let todoAdd = $("input[type='text']").val();
+		$(this).val("");
 		$("ul").append("<li><span class='check'><i class='fas fa-check'></i></span>"+todoAdd+"<span class='delete'><i class='fas fa-times'></i></span></li>");
 		});
 		$("input[type='text']").keypress(function(event){
@@ -60,4 +28,4 @@ $("input[type='text']").ready(function() {
 			}
 	});
 });
-*/
+
